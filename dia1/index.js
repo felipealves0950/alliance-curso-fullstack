0fd432b0e126560ccb6bfb463867c76bac71759b
felipe alves <feh0950@gmail.com>
2019-12-17 17:10:34 -0300

    dia2

--- dia1/index.js
+++ dia1/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import calculadora from './calculadora';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use('/calculadora', calculadora);
 
 app.get('/axios.js', (req, res) => res.sendFile(__dirname + '/axios.js'));
